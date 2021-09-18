6e539e887c2e896bde2de4de30bcdb4fac057b94
soulmachine <soulmachine@gmail.com>
2021-09-18 12:38:42 -0700

    temporary commit

---
+++ src/bin/crypto-daily-processor.rs
@@ -0,0 +1,16 @@
+use crypto_msg_parser::{MarketType, MessageType};
+
+fn process_files_of_day(
+    exchange: &str,
+    market_type: MarketType,
+    msg_type: MessageType,
+    day: &str,
+    input_dir: &str,
+    output_dir: &str,
+) -> (i64, i64) {
+    (0, 0)
+}
+
+fn main() {
+    println!("Hello, world!");
+}
