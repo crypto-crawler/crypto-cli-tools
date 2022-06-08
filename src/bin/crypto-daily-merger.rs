cace542c5c9bc9c7d0baa1501624653ac6cc24ed
soulmachine <soulmachine@gmail.com>
2022-06-08 04:41:53 +0000

    Bugfix: deribit open_interest has MarketType::Unknown

--- src/bin/crypto-daily-merger.rs
+++ src/bin/crypto-daily-merger.rs
@@ -637,7 +637,10 @@ fn process_files_of_day(day: &str, input_dirs: &[&str], output_dir: &str) -> boo
         }
 
         for path in paths.iter() {
-            assert!(!path.as_path().to_str().unwrap().contains(".unknown."));
+            let path_str = path.as_path().to_str().unwrap();
+            if !(exchange == "deribit" && msg_type == MessageType::OpenInterest) {
+                assert!(!path_str.contains(".unknown."), "{}", path_str);
+            }
         }
         let total_files = paths.len();
 
